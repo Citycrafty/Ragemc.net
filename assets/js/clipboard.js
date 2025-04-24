98b2b2d70b43248a533a72724212dca84b659312
Citycrafty <105089147+Citycrafty@users.noreply.github.com>
2025-04-24 20:06:54 +0530

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,10 @@
+function copy(){
+    navigator.clipboard.writeText('play.ragemc.online:19224')
+    swal({
+    title: 'IP COPIED',
+    text: 'IP COPIED SUCCESSFULLY',
+    icon: 'success',
+    timer: 2000,
+    buttons: false,
+})
+}
